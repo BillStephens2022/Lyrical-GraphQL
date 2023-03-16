a0ac5ce013f92727f1d0e7c316e76bf33ef54eb8
BillStephens2022 <two4onebill@yahoo.com>
2023-03-16 11:50:37 -0400

    added a form for adding a song

--- client/index.js
+++ client/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import { Router, Route, hashHistory, IndexRoute } from "react-router";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
-import App from './components/App';
+import App from "./components/App";
 import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
 
 const client = new ApolloClient({});
 
@@ -14,6 +15,7 @@ const Root = () => {
       <Router history={hashHistory}>
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
+          <Route path="song/new" component={SongCreate} />
         </Route>
       </Router>
     </ApolloProvider>
