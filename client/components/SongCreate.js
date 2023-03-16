0ce6123b8ebfb74a66a0c09a6294236ee46b44af
BillStephens2022 <two4onebill@yahoo.com>
2023-03-16 13:10:34 -0400

    added functionality to add song and navigate back to song list

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { Link, hashHistory } from "react-router";
 
 class SongCreate extends Component {
   constructor(props) {
@@ -8,11 +11,17 @@ class SongCreate extends Component {
 
   onSubmit(event) {
     event.preventDefault();
+    this.props.mutate({
+      variables: {
+        title: this.state.title,
+      },
+    }).then(() => hashHistory.push('/'));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
@@ -26,4 +35,12 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
