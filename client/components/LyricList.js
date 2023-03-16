1df90d1cca26764b2d46e031a55eddca9fc3f611
BillStephens2022 <two4onebill@yahoo.com>
2023-03-16 17:54:29 -0400

    updated like counter with Apollo optimistic responses

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { optimistic } from "apollo-client/optimistic-data/store";
 
 class LyricList extends Component {
-  onLike(id) {
-    this.props.mutate({ variables: { id } });
+  onLike(id, likes) {
+    this.props.mutate({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        likeLyric: {
+          id: id,
+          __typename: "LyricType",
+          likes: likes + 1,
+        },
+      },
+    });
   }
 
   renderLyrics() {
@@ -17,10 +28,13 @@ class LyricList extends Component {
         <li key={id} className="collection-item">
           {content}
           <div className="vote-box">
-          <i className="material-icons" onClick={() => this.onLike(id)}>
-            thumb_up
-          </i>
-          {likes}
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id, likes)}
+            >
+              thumb_up
+            </i>
+            {likes}
           </div>
         </li>
       );
